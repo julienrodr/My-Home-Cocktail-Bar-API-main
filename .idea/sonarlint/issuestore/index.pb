9c46fda614773ba0c1e1367d568c1b1d8e26822a
julienrodr <julien.rodriguez@cgi.com>
2022-04-01 18:50:10 +0200

    heroku init

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,3 +0,0 @@
-
-4
-.env,3/c/3c84dcdc6bbe3d7817c49dcdc327b926fea1808a
--- app/database.js
+++ app/database.js
@@ -1,5 +1,10 @@
 const { Pool } = require('pg')
 
-const db = new Pool();
+const db = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 module.exports = db;
